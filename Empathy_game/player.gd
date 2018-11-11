2933d85d6bc0e90d7fb6fd68dc670f3e4794fbdf
Josh Miller <miller.josh@husky.neu.edu>
2018-11-11 13:01:16 -0500

    player movement update

--- Empathy_game/player.gd
+++ Empathy_game/player.gd
@@ -6,7 +6,8 @@ extends KinematicBody2D
 # Member variables
 const MOTION_SPEED = 160 # Pixels/second
 
-
+var input_sequence = ""
+var last_input = ""
 
 
 func _physics_process(delta):
@@ -14,12 +15,28 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_up"):
 		motion += Vector2(0, -1)
+		if last_input != "U":
+			last_input = "U"
+			input_sequence += "U"
+			print(input_sequence)
 	if Input.is_action_pressed("move_bottom"):
 		motion += Vector2(0, 1)
+		if last_input != "D":
+			last_input = "D"
+			input_sequence += "D"
+			print(input_sequence)
 	if Input.is_action_pressed("move_left"):
 		motion += Vector2(-1, 0)
+		if last_input != "L":
+			last_input = "L"
+			input_sequence += "L"
+			print(input_sequence)
 	if Input.is_action_pressed("move_right"):
 		motion += Vector2(1, 0)
+		if last_input != "R":
+			last_input = "R"
+			input_sequence += "R"
+			print(input_sequence)
 	
 	motion = motion.normalized() * MOTION_SPEED
 
