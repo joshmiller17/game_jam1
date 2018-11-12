6c67d7511dc52896647b279c055c471a7e6cde5b
Clarence <thomasravelg@gmail.com>
2018-11-11 19:54:16 -0500

    Add timer back

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,8 +13,6 @@ var last_input = ""
 var necessary_input = ""
 var character = "A"
 var indicators = []
-var near_interaction_object = false
-var interaction_object 
 #var vis_indicator = preload("res://vis_indicator.tscn")
 
 func _ready():
@@ -25,7 +23,10 @@ func can_move(dir):
 	return necessary_input == "" or necessary_input.substr(0,1) == dir;
 	
 func update_move(dir):
-	clear_indicators()
+#	clear_indicators()
+	if character == "A":
+		pass
+		
 	if last_input != dir:
 		last_input = dir
 		input_sequence += dir
@@ -40,21 +41,30 @@ func update_move(dir):
 #			indicator_ref.get_ref().queue_free()
 #			indicators.erase(indicator_ref)
 
-#func show_where_to_go():
+func show_where_to_go():
 #	clear_indicators()
-#	var dir_needed = necessary_input.substr(0,1)
+	var dir_needed = necessary_input.substr(0,1)
 #	var ind = vis_indicator.instance()
 #	indicators.append(weakref(ind))
 #	add_child(ind)
-#	if dir_needed == "U":
+	var m = Vector2()
+	if dir_needed == "U":
+		m.x=0
+		m.y=-1
 #		ind.set_position(Vector2(0, -50))
-#	if dir_needed == "L":
+	if dir_needed == "L":
+		m.x=-1
+		m.y=0
 #		ind.set_position(Vector2(-50, 0))
-#	if dir_needed == "D":
+	if dir_needed == "D":
+		m.x=0
+		m.y=1
 #		ind.set_position(Vector2(0, 50))
-#	if dir_needed == "R":
+	if dir_needed == "R":
+		m.x=1
+		m.y=0
 #		ind.set_position(Vector2(50, 0))
-	
+	show_direction(m)
 
 func check_compulsions():
 	if input_sequence.ends_with("LR") and necessary_input == "":
@@ -85,29 +95,28 @@ func _physics_process(delta):
 	var motion = Vector2()
 	if Input.is_action_pressed("move_up") and can_move("U"):
 		motion += Vector2(0, -1)
-#		update_move("U")
-#	else:
-#		show_where_to_go()
+		update_move("U")
+	else:
+		show_where_to_go()
 	if Input.is_action_pressed("move_bottom") and can_move("D"):
 		motion += Vector2(0, 1)
-#		update_move("D")
-#	else:
-#		show_where_to_go()
+		update_move("D")
+	else:
+		show_where_to_go()
 	if Input.is_action_pressed("move_left") and can_move("L"):
 		motion += Vector2(-1, 0)
-#		update_move("L")
-#	else:
-#		show_where_to_go()
+		update_move("L")
+	else:
+		show_where_to_go()
 	if Input.is_action_pressed("move_right") and can_move("R"):
 		motion += Vector2(1, 0)
-#		update_move("R")
-#	else:
-#		show_where_to_go()
+		update_move("R")
+	else:
+		show_where_to_go()
 	
 	if motion.length()>0:
 		if character == "A":
 			$AnimatedSprite.animation = "a_walk"
-			$AnimatedSprite.play()
 		elif character == "B":
 			$AnimatedSprite.animation = "b_walk"
 		$AnimatedSprite.play()
@@ -118,24 +127,15 @@ func _physics_process(delta):
 			$AnimatedSprite.animation = "b_static"
 		$AnimatedSprite.stop()
 		
+	print(motion)
 		
-	if motion.x<0 && motion.y==0:
-		# left
-		$AnimatedSprite.rotation_degrees = 0
-		$AnimatedSprite.flip_h = true
-	elif motion.x>0 && motion.y==0:
-		$AnimatedSprite.rotation_degrees = 0
-		$AnimatedSprite.flip_h = false
+	show_direction(motion)
 	
-	elif motion.y<0 && motion.x==0:
-		#up
-		$AnimatedSprite.rotation_degrees = -90
-	elif motion.y>0 && motion.x==0:
-		$AnimatedSprite.rotation_degrees = 90
+	
 	
 	if Input.is_action_just_pressed('interact'):
-			if near_interaction_object:
-				interaction_object.interact(character)
+			$Message.show()
+			$MessageTimer.start()
 			
 	if character == "B":
 		check_compulsions()
@@ -148,11 +148,10 @@ func _physics_process(delta):
 	motion = motion.normalized() * MOTION_SPEED
 
 	move_and_slide(motion)
-	if(get_slide_collision(0) != null):
-		if (get_slide_collision(0).get_collider()).is_class('StaticBody2D'):
-			interaction_object = get_slide_collision(0).get_collider()
-			near_interaction_object = true
-		
+	#if(get_slide_collision(0) != null):
+	#	if (get_slide_collision(0).get_collider()).is_class('RigidBody2D'):
+	#		var msg = get_slide_collision(0).get_collider().msg
+	#		show_msg(msg)
 
 func _on_MessageTimer_timeout():
 	$Message.hide()
@@ -166,3 +165,17 @@ func leave_room():
 		show()
 	#if character == "B":
 	#	get_tree().quit()
+
+func show_direction(motion):
+	if motion.x<0 && motion.y==0:
+		# left
+		$AnimatedSprite.rotation_degrees = 0
+		$AnimatedSprite.flip_h = true
+	elif motion.x>0 && motion.y==0:
+		$AnimatedSprite.rotation_degrees = 0
+		$AnimatedSprite.flip_h = false
+	elif motion.y<0 && motion.x==0:
+		#up
+		$AnimatedSprite.rotation_degrees = -90
+	elif motion.y>0 && motion.x==0:
+		$AnimatedSprite.rotation_degrees = 90
