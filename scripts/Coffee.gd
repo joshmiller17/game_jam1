a6b3b16141cac66b7ba5fe11c1581cd30edbc9a7
Abdelrahman Madkour <a3madkour@gmail.com>
2018-11-11 19:28:39 -0500

    interaction is working with coffee now

--- scripts/Coffee.gd
+++ scripts/Coffee.gd
@@ -4,25 +4,30 @@ extends StaticBody2D
 # var a = 2
 # var b = "textvar"
 var table = 'empty'
-
+var mug_cleared = true
+var stain_visiable = false
 func show_mug():
 	$Mug.show()
-	$MugCollision.disabled = false
 	
 func show_stain():
 	$Mug.hide()
-	$MugCollision.disabled = true
 	$Stain.show()
-	$StainCollision.disabled = false
 
+func clear_stain():
+	$Stain.hide()
+	
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	$Mug.hide()
-	$MugCollision.disabled = true
 	$Stain.hide()
-	$StainCollision.disabled = true
-
+func interact(type):
+	if type == 'A' and mug_cleared:
+		show_mug()
+	elif type == 'B' and !mug_cleared:
+		show_stain()
+	elif type == 'B' and !stain_visiable:
+		clear_stain()
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
