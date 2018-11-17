7f5a72143b29421a892ff6ad77f43bdbf8874797
Josh Miller <miller.josh@husky.neu.edu>
2018-11-17 13:31:25 -0500

    reoganizing HUD

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,18 +15,11 @@ var character = "A"
 var indicators = []
 var near_interaction_object = false
 var interaction_object
-var hour = 8
-var minute = 30
-var pm = false
-var late = false
-var timer_started = false
 
-var flash = false
-var b_red = true
 #var vis_indicator = preload("res://vis_indicator.tscn")
 
 func _ready():
-	$HUD/Message.hide()
+	$Message.hide()
 	pass
 
 func can_move(dir):
@@ -98,8 +91,8 @@ func make_string_sequence(s):
 	return string
 
 func show_msg(msg):
-	$HUD/Message.show()
-	$HUD/MessageTimer.start()
+	$Message.show()
+	$MessageTimer.start()
 
 func _physics_process(delta):
 	var motion = Vector2()
@@ -167,7 +160,7 @@ func _physics_process(delta):
 	
 
 func _on_MessageTimer_timeout():
-	$HUD/Message.hide()
+	$Message.hide()
 	
 func leave_room():
 	if character == "A":
@@ -196,32 +189,3 @@ func show_direction(motion):
 		
 		
 		
-
-func _on_clock_timer_timeout():
-	minute += 1
-	if minute > 59:
-		minute = 0
-		hour += 1
-		# we're late
-		$Clock.add_color_override("font_color", Color(1,.2,.2,1)) 
-	if hour > 8 and minute > 30:
-		late = true
-	if late and not timer_started: # we're very late
-		$flash_timer.start()
-		timer_started = true
-	if hour > 12:
-		hour = 0
-		pm = not pm
-	if pm:
-		$Clock.text = "%02d:%02d PM" % [hour, minute]
-	else:
-		$Clock.text = "%02d:%02d AM" % [hour, minute]
-
-func _on_flash_timer_timeout():
-	b_red = !b_red
-	if b_red:
-		$Clock.add_color_override("font_color", Color(1,0,0,1)) 
-		
-	else:
-		$Clock.add_color_override("font_color", Color(1,1,1,1)) 
-	pass # replace with function body
